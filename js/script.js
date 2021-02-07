9c03393fe7e77db3672e8462cd369a3e38363447
NitePhoenix <NitePhoenix@users.noreply.github.com>
2021-02-07 01:21:31 -0500

    Added thunderstorm

--- js/script.js
+++ js/script.js
@@ -1,126 +1,193 @@
-var scene,
-    camera,
-    renderer,
-    geometry,
-    group;
+// declarations for tree
+var scene, camera, renderer, geometry, group;
 
+// declarations for sky
+var renderer, cloudParticles = [], flash, rain, rainGeo, rainCount = 15000;
 function init() {
-	// gotta shtick it in html somewhere
-	/*
-	var container1 = document.createElement( 'div' );
-	container1.setAttribute("id", "question");
-    document.body.appendChild( container1 );
 
-    var container2 = document.createElement( 'div' );
-	container2.setAttribute("id", "answer");
-    document.body.appendChild( container2 );
-    */
-
+	// start a new scene
 	scene = new THREE.Scene();
-	// show axes at the origin
+	/*
+	// show axes at the origin, guides
 	var axes = new THREE.AxesHelper(10);
-    scene.add(axes);
+	scene.add(axes);
+	*/
 
-  camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 10 );
-  camera.position.z = 5;
+	/*
+	// camera for sky
+	camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.z = 1;
+	camera.rotation.x = 1.16;
+	camera.rotation.y = -0.12;
+	camera.rotation.z = 0.27;
+	*/
 
-  boxgeometry = new THREE.BoxGeometry( 1, 1, 1 );
-  spheregeometry = new THREE.SphereGeometry( 1 );
-  cylindergeometry= new THREE.CylinderGeometry( 10, 10, 1, 20 );
+	// camera for tree
+	camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.z = 5;
 
-  // tree colours
-  var leaveDarkMaterial = new THREE.MeshLambertMaterial( { color: 0x00cc00 } );
-  var leaveLightMaterial = new THREE.MeshLambertMaterial( { color: 0x66ff66 } );
-  var leaveDarkDarkMaterial = new THREE.MeshLambertMaterial( { color: 0x006600 } );
-  var stemMaterial = new THREE.MeshLambertMaterial( { color: 0x7D5A4F } );
+    // scene lighting
+	ambient = new THREE.AmbientLight(0x555555);
+	// scene.add(ambient);
+	directionalLight = new THREE.DirectionalLight(0xffeedd); // represents moon
+	directionalLight.position.set(0,0,1);
+	scene.add(directionalLight);
 
-  //var cloudDarkMaterial = new 
+	// tree shapes
+	boxgeometry = new THREE.BoxGeometry( 1, 1, 1 );
+	spheregeometry = new THREE.SphereGeometry( 1 );
+	cylindergeometry= new THREE.CylinderGeometry( 10, 10, 1, 20 );
 
+	// tree colours
+	var leaveDarkMaterial = new THREE.MeshLambertMaterial( { color: 0x00cc00 } );
+	var leaveLightMaterial = new THREE.MeshLambertMaterial( { color: 0x66ff66 } );
+	var leaveDarkDarkMaterial = new THREE.MeshLambertMaterial( { color: 0x006600 } );
+	var stemMaterial = new THREE.MeshLambertMaterial( { color: 0x7D5A4F } );
+
+	// tree parts
+	var stem = new THREE.Mesh( boxgeometry, stemMaterial );
+	stem.position.set( 0, 0, 0 );
+	stem.scale.set( 0.3, 1.5, 0.3 );
+	var leaf01 = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
+	leaf01.position.set( 0.4, 1.6, 0.5 );
+	leaf01.scale.set( 0.8, 0.8, 0.9 );
+	var leaf02 = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
+	leaf02.position.set( -0.4, 1.3, -0.4 );
+	leaf02.scale.set( 0.7, 0.7, 0.7 );
+	var leaf03 = new THREE.Mesh( spheregeometry, leaveLightMaterial );
+	leaf03.position.set( 0.4, 1.7, -0.5 );
+	leaf03.scale.set( 0.7, 0.7, 0.7 );
+	var leaf04 = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
+	leaf04.position.set( 0, 2, 0 );
+	leaf04.scale.set( 0.7, 0.7, 0.7 );
+	var leaveDark = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
+	leaveDark.position.set( 0, 1.2, 0 );
+	leaveDark.scale.set( 0.3, 1, 0.3 );
+	var leaveLight = new THREE.Mesh( spheregeometry, leaveLightMaterial );
+	leaveLight.position.set( 0, 1.2, 0 );
+	leaveLight.scale.set( 0.5, 0.5, 0.5 );
+	// ground
+	var ground = new THREE.Mesh( cylindergeometry, leaveDarkDarkMaterial );
+	ground.position.set( 0, -1, 0 );
+	//ground.scale.set( 10, 0.8, 10 ); // ground already scaled
+
+	// group tree parts
+	tree = new THREE.Group();
+	tree.add( leaveDark );
+	tree.add( leaveLight );
+	tree.add( leaf01 );
+	tree.add( leaf02 );
+	tree.add( leaf03 );
+	tree.add( leaf04 );
+	tree.add( ground );
+	tree.add( stem );
+
+	// lil' rotatey
+	tree.rotation.y = 1;
+	tree.rotation.x = 0.5;
+
+	scene.add( tree );
+
+	// lightning in cloud
+	flash = new THREE.PointLight(0x062d89, 30, 500 ,1.7);
+	flash.position.set(200,300,100);
+	scene.add(flash);
+
+	renderer = new THREE.WebGLRenderer();
+	scene.fog = new THREE.FogExp2(0x11111f, 0.002);
+	renderer.setClearColor(scene.fog.color);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	document.body.appendChild(renderer.domElement);
+
+	// let it rain
+	rainGeo = new THREE.Geometry(); // ISSUES IF USING UPDATED THREE.JS LIBRARY BEGIN HERE
+	for(let i=0;i<rainCount;i++) {
+		rainDrop = new THREE.Vector3(
+			Math.random() * 400 -200,
+			Math.random() * 500 - 250,
+			Math.random() * 400 - 200
+			);
+		rainDrop.velocity = {};
+		rainDrop.velocity = 0;
+		rainGeo.vertices.push(rainDrop);
+	}
+	rainMaterial = new THREE.PointsMaterial({
+		color: 0xaaaaaa,
+		size: 0.1,
+		transparent: true
+	});
+	rain = new THREE.Points(rainGeo,rainMaterial);
+	scene.add(rain);
+
+	// add cloud
+	let loader = new THREE.TextureLoader();
+	loader.load("https://www.designer-illusions.com/3d-experiments/rain/smoke.png", function(texture){
+		cloudGeo = new THREE.PlaneBufferGeometry(500,500);
+		cloudMaterial = new THREE.MeshLambertMaterial({
+			map: texture,
+			transparent: true
+		});
+		for(let p=0; p<25; p++) {
+			let cloud = new THREE.Mesh(cloudGeo,cloudMaterial);
+			cloud.position.set( 
+				Math.random()*800 -400,
+				500,
+				Math.random()*500 - 450
+				);
+			cloud.rotation.x = 1.16;
+			cloud.rotation.y = -0.12;
+			cloud.rotation.z = Math.random()*360;
+			cloud.material.opacity = 0.6;
+			cloudParticles.push(cloud);
+			scene.add(cloud);
+		}
+		animate();
+	});
+} // end init
+
+function animate() {
+	cloudParticles.forEach(p => {
+		p.rotation.z -=0.002;
+	});
+	rainGeo.vertices.forEach(p => {
+		p.velocity -= 0.1 + Math.random() * 0.1;
+		p.y += p.velocity;
+		if (p.y < -200) {
+			p.y = 200;
+			p.velocity = 0;
+		}
+	});
+	rainGeo.verticesNeedUpdate = true;
+	rain.rotation.y +=0.002;
+	if(Math.random() > 0.93 || flash.power > 100) {
+		if(flash.power < 100) 
+			flash.position.set(
+				Math.random()*400,
+				300 + Math.random() *200,
+				100
+				);
+		flash.power = 50 + Math.random() * 500;
+	}
+
+  // render my scene
   /*
-  var light = new THREE.AmbientLight( 0xc9c9c9 ); // soft white light
-  scene.add( light );
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
   */
-
-  var light = new THREE.DirectionalLight( 0xEEFFD3, 1 );
-  light.position.set( 0, 0, 1 );
-  scene.add( light );
-
-  var light = new THREE.DirectionalLight( 0xFF0000, 0.4 );
-  light.position.set( 1, 0, 0 );
-  scene.add( light );
-
-  var light = new THREE.DirectionalLight( 0xFFFFFF, 0.2 );
-  light.position.set( 0, 1, 0 );
-  scene.add( light );
-  
-
-  var stem = new THREE.Mesh( boxgeometry, stemMaterial );
-  stem.position.set( 0, 0, 0 );
-  stem.scale.set( 0.3, 1.5, 0.3 );
-
-
-  var leaf01 = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
-  leaf01.position.set( 0.4, 1.6, 0.5 );
-  leaf01.scale.set( 0.8, 0.8, 0.9 );
-
-  var leaf02 = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
-  leaf02.position.set( -0.4, 1.3, -0.4 );
-  leaf02.scale.set( 0.7, 0.7, 0.7 );
-
-  var leaf03 = new THREE.Mesh( spheregeometry, leaveLightMaterial );
-  leaf03.position.set( 0.4, 1.7, -0.5 );
-  leaf03.scale.set( 0.7, 0.7, 0.7 );
-
-  var leaf04 = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
-  leaf04.position.set( 0, 2, 0 );
-  leaf04.scale.set( 0.7, 0.7, 0.7 );
-
-  var leaveDark = new THREE.Mesh( spheregeometry, leaveDarkMaterial );
-  leaveDark.position.set( 0, 1.2, 0 );
-  leaveDark.scale.set( 0.3, 1, 0.3 );
-
-  var leaveLight = new THREE.Mesh( spheregeometry, leaveLightMaterial );
-  leaveLight.position.set( 0, 1.2, 0 );
-  leaveLight.scale.set( 0.5, 0.5, 0.5 );
-
-  var ground = new THREE.Mesh( cylindergeometry, leaveDarkDarkMaterial );
-  ground.position.set( 0, -1, 0 );
-  //ground.scale.set( 10, 0.8, 10 ); // ground already scaled
-
-  tree = new THREE.Group();
-  tree.add( leaveDark );
-  tree.add( leaveLight );
-  tree.add( leaf01 );
-  tree.add( leaf02 );
-  tree.add( leaf03 );
-  tree.add( leaf04 );
-  tree.add( ground );
-  tree.add( stem );
-
-  tree.rotation.y = 1;
-  tree.rotation.x = 0.5;
-
-  scene.add( tree );
-
-  renderer =  new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
-  document.body.appendChild( renderer.domElement );
-
   render();
 }
 
 function render() {
-  requestAnimationFrame( render );
-  // handles rotation
+  requestAnimationFrame( animate );
+  // handles rotation of tree
   // tree.rotation.y += 0.007;
   renderer.render( scene, camera );
 }
 
 // handles resizing window
 function onResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 	// If we use a canvas then we also have to worry of resizing it
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
@@ -128,3 +195,4 @@ window.addEventListener('resize', onResize, true);
 
 // start!
 window.onload = init;
+
